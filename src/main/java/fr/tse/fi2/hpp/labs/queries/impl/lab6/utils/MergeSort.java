d96674a4de8f700485ece85fdf6236a65e793baa
yishuo <yishuo.lyu@gmail.com>
2015-05-19 13:50:16 +0200

    TP6:test corrigé

--- src/main/java/fr/tse/fi2/hpp/labs/queries/impl/lab6/utils/MergeSort.java
+++ src/main/java/fr/tse/fi2/hpp/labs/queries/impl/lab6/utils/MergeSort.java
@@ -1,49 +1,125 @@
 package fr.tse.fi2.hpp.labs.queries.impl.lab6.utils;
 
-public class MergeSort
-{
-	public static void merge(int a[],int pl,int mid,int pr)
+import java.util.Random;
+
+public class MergeSort {
+
+	//obtenir la liste inclus les valeurs aleatoires
+	public static int[] genererAleatoire(int n){
+		int[] tableau = new int[n];
+		Random random = new Random();
+
+		for(int i=0 ; i<tableau.length ; i++){
+			int valeur = random.nextInt();
+			tableau[i] = valeur;
+		}
+
+		return tableau;
+	}
+	
+	public static int[] trier (int[] liste){
+
+		int len = liste.length;
+
+		if (len > 1){
+
+			int[] sousListe1 = trier(cutArrayList(liste, 0, len/2));
+			int[] sousListe2 = trier(cutArrayList(liste, len/2, len));
+			
+			int a = 0;
+			int b = 0;
+
+			for(int i = 0 ; i<len ; i++){
+
+				if( a<sousListe1.length && ((b >= sousListe2.length) || (sousListe1[a] < sousListe2[b]) )){
+					liste[i] = sousListe1[a];
+					a++;
+				} else if(b < sousListe2.length){
+
+					liste[i] = sousListe2[b];
+					b++;
+				}		
+			}			
+		}
+		return liste;
+	}
+
+	public static int[] trierInsertionSort (int[] liste){
+
+		int len = liste.length;
+
+		if (len > 20){
+
+			int[] sousListe1 = trierInsertionSort(cutArrayList(liste, 0, len/2));
+			int[] sousListe2 = trierInsertionSort(cutArrayList(liste, len/2, len));
+			
+			int a = 0;
+			int b = 0;
+
+			for(int i = 0 ; i<len ; i++){
+
+				if( a<sousListe1.length && ((b >= sousListe2.length) || (sousListe1[a] < sousListe2[b]) )){
+					liste[i] = sousListe1[a];
+					a++;
+				} else if(b < sousListe2.length){
+
+					liste[i] = sousListe2[b];
+					b++;
+				}		
+			}			
+		} else {
+			InsertionSort(liste);
+		}
+
+		return liste;
+	}
+
+	public static int[] cutArrayList(int[] liste, int debut, int fin){
+		int[] tab = new int[(fin - debut)];
+
+		int j = 0;
+		for(int i=debut ; i<fin ; i++){
+			tab[j] = liste[i];	
+			j++;
+		}
+		return tab;
+	}
+
+	public static void InsertionSort(int[] num)
 	{
-		int temp[]=new int[pr-pl+1];
-		int i=0,left=pl,right=mid+1;
-		while(left<=mid && right<=pr)
+		int j;                  // the number of items sorted so far
+		int key;                // the item to be inserted
+		int i; 
+
+		for (j = 1; j < num.length; j++)    // Start with 1 (not 0)
 		{
-			temp[i++]=a[left]<a[right] ? a[left++]:a[right++];
+			key = num[ j ];
+			for(i = j - 1; (i >= 0) && (num[ i ] > key); i--)  
+			{
+				num[ i+1 ] = num[ i ];
+			}
+			num[ i+1 ] = key;    // Put the key in its proper location
 		}
-		while(left<=mid)
-		{
-			temp[i++]=a[left++];
+	}
+
+	
+	public static void main(String[] args) {
+
+		int[] liste = MergeSort.genererAleatoire(10000);
+
+		System.out.println("La liste originale : ");
+		for(int i=0 ; i<liste.length ; i++){
+			System.out.println(liste[i]);
 		}
-		while(right<=pr)
-		{
-			temp[i++]=a[right++];
-		}
-		for(int j=0,k=pl;k<=pr;j++,k++)
-		{
-			a[k]=temp[j];
+
+		int[] listeTrie = MergeSort.trier(liste);
+
+		System.out.println("La nouvelle liste après sort");
+		for(int i=0 ; i<listeTrie.length ; i++){
+			System.out.println(listeTrie[i]);
 		}
 
 	}
-	public static void sort(int a[],int left,int right)
-	{
-		int mid;
-		if(left<right)
-		{
-			mid=(left+right)/2;
-			sort(a,left,mid);
-			sort(a,mid+1,right);
-			merge(a,left,mid,right);
-		}
-	}
-/*	
- *public static void main(String args[])
-	{
-		int a[]={26, 5, 98, 108, 28, 99, 100, 56, 34, 1 };
-		MergeSort.sort(a, 0, a.length-1);
-		for(int i=0;i<a.length;i++)
-		{
-			System.out.println(a[i]);
-		}
-	}
-*/	
+	
+
 }
--- src/test/java/fr/tse/fi2/hpp/labs/test/TestSorting.java
+++ src/test/java/fr/tse/fi2/hpp/labs/test/TestSorting.java
@@ -2,51 +2,44 @@ package fr.tse.fi2.hpp.labs.test;
 
 import static org.junit.Assert.*;
 
+import java.util.Arrays;
+
 import org.junit.Test;
 
-import fr.tse.fi2.hpp.labs.queries.impl.lab6.utils.InsertionSort;
-import fr.tse.fi2.hpp.labs.queries.impl.lab6.utils.ListeRandom;
 import fr.tse.fi2.hpp.labs.queries.impl.lab6.utils.MergeSort;
 
 public class TestSorting {
-	
-	int[] liste = ListeRandom.ListeRandom(); 
-	int[] a = liste;
-	int[] b = liste;
-	int[] c = liste;
 
 	@Test
-	public void testMergeSort() {
-		//int[] a = {26, 5, 98, 108, 28, 99, 100, 56, 34, 1 };
-		long starttime1=System.nanoTime();	 
-//        MergeSort.printArray("Avant Sort : ", a);
-        MergeSort.sort(a, 0, a.length-1);
-//        System.out.println();
-//        MergeSort.printArray("Après Sort : ", a);
-        long endtime1=System.nanoTime();	 
-		System.out.println("1.Time (merge) taken by program:"+(endtime1-starttime1)+"ns");
+	public void test1() {
+		
+		int[] tab1;
+		int[] tab2;
+		
+		tab1 = MergeSort.genererAleatoire(100000);
+		tab2 = tab1;
+		
+		Arrays.sort(tab1);
+		
+		int[] listeTrie1 = MergeSort.trier(tab2);
+		
+		assertArrayEquals(tab1, listeTrie1);
 	}
 	
 	@Test
-	public void testInsertionSort() {
-		//int[] a = {26, 5, 98, 108, 28, 99, 100, 56, 34, 1 };
-		long starttime2=System.nanoTime();	 	
-		InsertionSort.InsertionSort(b);
-		long endtime2=System.nanoTime();	 
-		System.out.println("2.Time (insertion) taken by program:"+(endtime2-starttime2)+"ns");
+	public void test2() {
 		
-	}
-	
-/*	@Test
-	public void test() {
-		//int[] a = {26, 5, 98, 108, 28, 99, 100, 56, 34, 1 };
-		long starttime3=System.nanoTime();	 	
-		for(int i=1;i<=100000;i++){	 
-			java.util.Arrays.sort(c);	
-			}	  
-		long endtime3=System.nanoTime();	 
-		System.out.println("3.Time taken by program:"+(endtime3-starttime3)+"ns");
+		int[] tab1;
+		int[] tab2;
 		
+		tab1 = MergeSort.genererAleatoire(100000);
+		tab2 = tab1;
+		
+		Arrays.sort(tab1);
+		
+		int[] listeTrie2 = MergeSort.trierInsertionSort(tab2);
+		
+		assertArrayEquals(tab1, listeTrie2);
 	}
-*/
+
 }
